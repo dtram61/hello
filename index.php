8c5381d32068884dacbd6f5001ee1962be7d677f
user.email <dtramgre@srv604.webhostingforstudents.com>
2022-04-12 13:42:23 -0400

    index has been changed

--- index.php
+++ index.php
@@ -11,10 +11,20 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 //Define a default route
-$f3->route('GET /', function() {
-    echo '<h1>Hello, world!</h1>';
-}
-);
+
+
+
+    //Define a default route
+    $f3->route('GET /', function() {
+        //echo '<h1>Hello, world!</h1>';
+        $view = new Template();
+
+        echo $view->render
+        ('views/home.html');
+    }
+    );
+
+
 
 
 //Run fat free
